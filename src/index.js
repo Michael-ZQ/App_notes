db02c5c66282f25dfc5f9bf1410b5e84e4ad0601
Michael <michael999mz@gmail.com>
2019-11-29 20:04:21 -0500

    se agrego los routes viwes u el npm run dev para iniciar

---
+++ src/index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const methodOverride = require('method-override');
+const session = require('express-session');
+
+//initiazations
+const app = express();
+ 
+//setings
+app.set('port', process.env.PORT  || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbos',exphbs({
+    defaultLayout:'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir:path.join(app.get('views'), 'partials'),
+    extname:'.hbs'
+}));
+app.set('view engine','.hbs'); // configurando las views
+
+
+//middlewares
+app.use(express.urlencoded({extended: false}));  // para entender datos de un formulario
+app.use(methodOverride('_method')); //sire para que los ofmularios puedan enviar otros tipos de metodos no solo get 
+app.use(session({
+    secret:'mysecret',
+    resave: true,
+    saveUninitialized: true
+})); // configuraciones por default , para que expres puedda allmazener lso datos temporalmente 
+
+
+
+
+//global variables
+
+//Routes
+app.use(require('./routes/index'));
+app.use(require('./routes/notes'));
+app.use(require('./routes/users'));
+
+
+//Static Files
+
+//Server is Listen
+app.listen(app.get('port'), ()=>{
+    console.log('Server on Port',app.get('port'));
+});
