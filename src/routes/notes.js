351a595fd6b5e1081ffe32872d4ae86e6891c7e7
Michael <michael999mz@gmail.com>
2019-12-01 23:42:06 -0500

    Todo listo en la ruta notas para empesar a guardar en la base de datos

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,6 +1,36 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/notes/add',(req,res)=>{
+    res.render('notes/addNotes');
+});
+
+router.post('/notes/addNotes', (req, res)=>{
+     const {title, description} = req.body;
+     const errors = [];
+     if(!title){
+        errors.push({text:'Please Write a Title'});
+     }
+     if(!description){
+        errors.push({text:'Please Write a Description'});
+
+     }
+     if(errors.length > 0){
+        res.render('notes/addNotes', {
+            errors,
+            title,
+            description
+        });
+     }else{
+        res.send('ok');
+     }
+}); 
+//ruta a la cual el formulario enviara la info
+/* 
+POST consiste en datos "ocultos" (porque el cliente no los ve) 
+enviados por un formulario cuyo método de envío es post. Es adecuado para formularios. 
+Los datos no son visibles.
+*/
 
 router.get('/notes', (req, res)=>{
     res.send('Your Notes');
